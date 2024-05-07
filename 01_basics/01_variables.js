ac3b168b381fdd646a7d787502a5f7fc61b52343
kavya-k243 <89535516+kavya-k243@users.noreply.github.com>
2024-05-07 09:34:25 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 123
+let accountEmail = "ab@gmail.com"
+var accountPass = "123"
+accountCity = "Kota"
+
+// accountId = 23 // not allowed
+accountEmail = "bv@gmial.com"
+accountPass = "09"
+accountCity = "Jaipur"
+
+console.table([accountId, accountEmail, accountPass, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) // we r using node js not browser
+console.log(3+3)
+console.log("Hitesh")
+let name = "kavya"
+let age = 20
+let isLoggedIn = false 
+
+// number --> 2 to power 53
+// string --> ""
+// boolean --> true/false
+// null --> standalone value
+// undefined --> value not assigned
+// symbol --> unique
+console.log(typeof age)
